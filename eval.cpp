b231f09fc23dffe067adf1515fc351552294876e
sandraleeusc <sbatista@usc.edu>
2020-09-08 10:20:33 -0700

    Create eval.cpp

---
+++ eval.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
+using namespace std;
+
+//double evaluate(string exp);
+/** evaluate(“( ( 5 * 4 ) * (  2 + 3 ) )”) should return the value 100.0 **/
+/** evaluate(“(  (1 + ( 5 * ( 3 + 1 ) ) ) / 4 )”) should return the value 5.25 **/
+
+
+double evaluate(string expr){
+    stringstream s(expr);
+// this is not correct. fix here
+   return 0;
+}
+
+
+
+int main()
+{
+    string test = "( ( 1 + ( 5 * ( 3 + 1 ) ) ) / 4 )";
+    cout <<evaluate(test) << endl;
+    return 0;
+}
