48a22ab58bf2b44738776333e4cbee4a69638b26
sandraleeusc <sbatista@usc.edu>
2020-09-10 01:49:41 -0700

    Create double_test.cpp

---
+++ double_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "DoubleLL.h"
+using namespace std;
+
+int main(){
+    DoubleLL test;
+    test.push_front(5);
+    test.push_front(6);
+    test.push_front(7);
+    
+
+    cout << "start the destructor " << endl;
+}
