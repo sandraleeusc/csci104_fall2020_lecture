fc03138a29f05aa546ba27e8bbaf1d1cf5c78d68
sandraleeusc <sbatista@usc.edu>
2020-09-28 23:19:37 -0700

    Create CharBlockSort.cpp

---
+++ CharBlockSort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef struct CharacterBlock {
+
+    // The starting index of the block we're allocating in the buffer
+    char* address;
+
+    // The number of characters allocated to the memory block
+    size_t size;
+
+} CharacterBlock;
+
+// Write a comparator that will return true if and only if the
+// first CharacterBlock has a smaller physical address
+
+
+int main(){
+    char buffer[1000];
+    vector<CharacterBlock> a(3);
+    
+   a[0].address = buffer + 700;
+   a[0].size = 20;
+   a[1].address = buffer;
+   a[1].size = 100;
+   a[2].address = buffer+558;
+   a[2].size = 75;
+    
+    
+    for (int i = 0; i < 3; i++ ) {
+        cout << "CharBlock " << i << " starting address: " << static_cast<const void *> (a[i].address) << endl;
+    } 
+    cout << endl;
+    
+    // Sort the character blocks using your comparator and verify that it worked
+    
+    for (int i = 0; i < 3; i++ ) {
+        cout << "CharBlock " << i << " starting address: " << static_cast<const void *> (a[i].address) << endl;
+    } 
+    
+}
