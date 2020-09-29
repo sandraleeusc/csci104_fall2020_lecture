1324eb6a767b4c4057c33042bde114ddde97f2e3
sandraleeusc <sbatista@usc.edu>
2020-09-28 22:20:57 -0700

    Create max_comparator.cpp

---
+++ max_comparator.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+template <typename T, typename comp>
+   T mymax(const T& a, const T& b, comp test)
+{
+  // Implement your function to use your comparator
+   return b;
+}
+   
+   
+ // Write a comparator to compare the size of 2 vectors of ints and return the larger sized
+ // vector
+ 
+ 
+ // Write a comparator to compare the sum of two vectors of  ints and return
+ // the vector with the larger sum
+ 
+ 
+
+
+int main(){
+    vector<int> a(10);
+    vector<int> b(11);
+    
+    for (int i = 0; i < 10; i++ ) {
+        a[i] = 2*(i+1);
+        b[i] = i+1;
+    }
+    
+    b[10] = 11;
+
+     // Instantiate your size comparator
+     // Instantiate your sum comparator
+    // Test each comparator with the vectors a, b and the function max
+}
