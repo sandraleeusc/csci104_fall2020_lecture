c36be0d5ece2f9e444bb10c4413dbfdaf6fa35f3
sandraleeusc <sbatista@usc.edu>
2020-10-06 04:21:35 -0700

    Create heapsort.cpp

---
+++ heapsort.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <queue> 
+#include <stdlib.h>
+using namespace std; 
+
+int main () 
+{
+  
+
+  
+  // how to get a random integer between 0 and 1000
+      int k = rand()%1000;
+    
+  
+  cout<< "Sorting elements..." << endl;
+ // print integers in order from smallest to largest
+    
+    
+}
